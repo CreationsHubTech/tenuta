c11634d2a3e1a59f65d39166b10ce35d9ea99f54
Diego Hernandes <diegowalhernandes@gmail.com>
2024-12-29 11:23:34 -0300

    domenico pages

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-// Script básico para interatividade (exemplo)
-document.addEventListener("DOMContentLoaded", () => {
-    const learnMoreButton = document.querySelector("header a");
-    learnMoreButton.addEventListener("click", () => {
-        alert("Explore o que temos a oferecer na Tenuta dell'Annunziata!");
-    });
-});
